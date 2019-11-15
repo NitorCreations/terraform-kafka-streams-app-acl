9eb298e183279b803f91f0f465f5754ff0fd78ff
Mika Majakorpi <mika.majakorpi@nitor.com>
2019-11-15 10:42:12 +0200

    Rename user_id variable to principal for clarity

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
-variable "user_id" {
+variable "principal" {
   type = string
+  description = "The Kafka principal the ACL entry is for. Format is <principalType>:<principalName>. Examples: User:alice, User:1234"
 }
 
 variable "application_id" {
